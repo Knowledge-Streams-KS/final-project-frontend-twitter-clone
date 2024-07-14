28f0c5b2b1c459867515dc93e04787d8f984e418
Mujtaba <126389386+Mujtaba500@users.noreply.github.com>
2024-07-14 22:27:47 +0500

    Installed required dependencies

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <p className="text-black text-center mt-10">Project started</p>
+      <button className="btn btn-success">Success</button>
+    </>
+  );
+}
+
+export default App;
