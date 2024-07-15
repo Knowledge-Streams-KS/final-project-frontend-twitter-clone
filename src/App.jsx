32147f9966f961ff9e93a5d493288b4acf461528
Mujtaba <126389386+Mujtaba500@users.noreply.github.com>
2024-07-15 10:47:13 +0500

    Project setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <>
       <p className="text-black text-center mt-10">Project started</p>
-      <button className="btn btn-success">Success</button>
+      <button className="btn btn-primary">Success</button>
     </>
   );
 }
