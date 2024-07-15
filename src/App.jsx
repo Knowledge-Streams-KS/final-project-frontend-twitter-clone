16ec9c98097f51d7cc2904601f332eea7c02d9b7
Mujtaba <126389386+Mujtaba500@users.noreply.github.com>
2024-07-15 12:50:58 +0500

    Signup page using formik

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import HomePage from "./pages/home/HomePage.jsx";
+import SignUpPage from "./pages/auth/SignUpPage.jsx";
+import LoginPage from "./pages/auth/LoginPage.jsx";
+import { Routes, Route } from "react-router-dom";
+
 function App() {
   return (
-    <>
-      <p className="text-black text-center mt-10">Project started</p>
-      <button className="btn btn-primary">Success</button>
-    </>
+    <div className="flex max-w-6xl mx-auto">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </div>
   );
 }
 
---
+++ src/pages/auth/LoginPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LoginPage = () => {
+  return <div>LogInPage</div>;
+};
+
+export default LoginPage;
