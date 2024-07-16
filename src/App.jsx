b72b53ea0bc4bf5013eadd62d7ae152bf0757997
Mujtaba <126389386+Mujtaba500@users.noreply.github.com>
2024-07-16 11:56:38 +0500

    Components added

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,22 @@ import HomePage from "./pages/home/HomePage.jsx";
 import SignUpPage from "./pages/auth/SignUpPage.jsx";
 import LoginPage from "./pages/auth/LoginPage.jsx";
 import { Routes, Route } from "react-router-dom";
+import Sidebar from "./components/common/Sidebar.jsx";
+import RightPanel from "./components/common/RightPanel.jsx";
+import NotificationPage from "./pages/notification/NotificationPage.jsx";
 
 function App() {
   return (
     <div className="flex max-w-6xl mx-auto">
+      {/* common components */}
+      <Sidebar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/notifications" element={<NotificationPage />} />
       </Routes>
+      <RightPanel />
     </div>
   );
 }
